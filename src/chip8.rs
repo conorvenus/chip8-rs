29000f1d78f14b424bf38d30977306f38b28de9e
conorv <70652079+conorvenus@users.noreply.github.com>
2023-11-18 21:52:23 +0000

    Implemented all the opcodes from the basic IBM test, implemented display rendering

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,10 +1,12 @@
+use std::{fs, any};
+
 const WIDTH: u32 = 64;
 const HEIGHT: u32 = 32;
 
 pub struct Chip8 {
     memory: [u8; 4096],
     stack: [u16; 16],
-    display: [[u8; WIDTH as usize]; HEIGHT as usize],
+    pub display: [[u8; WIDTH as usize]; HEIGHT as usize],
     v_registers: [u8; 16],
     i_register: u16,
     pc: u16,
@@ -29,9 +31,60 @@ impl Chip8 {
         instruction | self.memory[(self.pc + 1) as usize] as u16
     }
 
+    fn execute_instruction(&mut self, instruction: u16) -> Result<(), String> {
+        self.pc += 2;
+        Ok(match instruction {
+            0x00E0 => self.display = [[0; WIDTH as usize]; HEIGHT as usize],
+            0xA000..=0xAFFF => self.i_register = instruction & 0x0FFF,
+            0x6000..=0x6FFF => {
+                let register_idx: u8 = ((instruction >> 8) as u8) & 0x0F;
+                let register_value: u8 = (instruction & 0x00FF) as u8;
+                self.v_registers[register_idx as usize] = register_value;
+            },
+            0xD000..=0xDFFF => {
+                let x_coord = ((instruction >> 8) as u8) & 0x0F;
+                let x_coord = self.v_registers[x_coord as usize];
+
+                let y_coord = ((instruction >> 4) & 0x000F) as u8;
+                let y_coord = self.v_registers[y_coord as usize];
+
+                let height = (instruction & 0x000F) as u8;
+                let mut any_set = false;
+
+                for i in 0..height {
+                    let row = self.memory[(self.i_register + (i as u16)) as usize];
+                    for j in 0..8 {
+                        let bit_j = (row >> (7 - j)) & 1;
+
+                        if bit_j != self.display[(y_coord + i) as usize][(x_coord + j) as usize] {
+                            any_set = true;
+                            self.display[(y_coord + i) as usize][(x_coord + j) as usize] = bit_j;
+                        }
+                    }
+                }
+
+                self.v_registers[0xF] = if any_set { 1 } else { 0 }; 
+            },
+            0x7000..=0x7FFF => {
+                let register_idx = (instruction >> 8) & 0x000F;
+                let value = instruction & 0x00FF;
+                self.v_registers[register_idx as usize] += value as u8;
+            },
+            0x1000..=0x1FFF => self.pc = instruction & 0x0FFF,
+            other => return Err(format!("Opcode 0x{:04X} is not defined", other))
+        }) 
+    }
+
     pub fn cycle(&mut self) {
         let instruction = self.fetch_instruction();
-        self.pc += 2;
+        self.execute_instruction(instruction).expect("opcode should be defined");
+    }
+    
+    pub fn load_rom(&mut self, filename: &str) {
+        if let Ok(contents) = fs::read(filename) {
+            for (idx, byte) in contents.iter().enumerate() {
+                self.memory[0x200 + idx] = *byte;
+            } 
+        }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::event::Event;
+
 mod chip8;
 use chip8::Chip8;
 
 pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("CHIP8 EMULATOR", 640, 320)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_logical_size(64, 32).unwrap();
+
     let mut emulator: Chip8 = Chip8::new();
-    loop {
+    emulator.load_rom("roms/IBM Logo.ch8");
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => break 'running,
+                _ => {}
+            }
+        }
+
         emulator.cycle();
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        for (y, row) in emulator.display.iter().enumerate() {
+            for (x, pixel) in row.iter().enumerate() {
+                if *pixel == 1 {
+                    canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+                }
+            }
+        }
+
+        canvas.present();
     }
 }

ca2df0b5cdda5cd7413f48a5ad8e2b6394957e06
conorv <70652079+conorvenus@users.noreply.github.com>
2023-11-18 22:07:49 +0000

    Refactored the main body of code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use sdl2::pixels::Color;
 use sdl2::rect::Point;
 use sdl2::event::Event;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 mod chip8;
 use chip8::Chip8;
 
-pub fn main() {
+fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let mut window = video_subsystem.window("CHIP8 EMULATOR", 640, 320)
-        .position_centered()
+    let window = video_subsystem
+        .window("CHIP8 EMULATOR", 640, 320)
         .fullscreen_desktop()
         .build()
         .unwrap();
@@ -24,26 +26,31 @@ pub fn main() {
 
     'running: loop {
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} => break 'running,
-                _ => {}
-            }
+            if let Event::Quit {..} = event { break 'running }
         }
 
         emulator.cycle();
 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
+        render_display(&emulator, &mut canvas);
+    }
+}
 
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        for (y, row) in emulator.display.iter().enumerate() {
-            for (x, pixel) in row.iter().enumerate() {
-                if *pixel == 1 {
-                    canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
-                }
+fn clear_display(canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+}
+
+fn render_display(emulator: &Chip8, canvas: &mut Canvas<Window>) {
+    clear_display(canvas);
+
+    for (y, row) in emulator.display.iter().enumerate() {
+        for (x, &pixel) in row.iter().enumerate() {
+            if pixel == 1 {
+                canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
             }
         }
-
-        canvas.present();
     }
+
+    canvas.present();
 }
